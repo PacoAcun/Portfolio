45306afbf600b1458a15f173c2426c6346fb106d
paco <103969393+PacoAcun@users.noreply.github.com>
2023-12-04 01:21:41 -0600

    Hero, 3d model and navbar

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,54 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { BakeShadows, OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
+const Computers = () => {
+  const computer = useGLTF('./cyber_desk/scene.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.75} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene}
+        scale={1.25}
+        position={[9, -6, 7]}
+        rotation={[0.1, -0, 0]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
